78d0a6ebc6ef26e8830d4af2c2fd27ccd6b3627d
emremidilli <70489609+emremidilli@users.noreply.github.com>
2020-09-21 23:02:12 +0300

    Update Train_Models.py

--- Train_Models.py
+++ Train_Models.py
@@ -12,11 +12,12 @@ from sklearn import metrics
 from Connect_to_Database import execute_sql
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+import pickle
 
 
 training_ratio = 0.7
 test_ratio = round(1 - training_ratio,2)
-
+scalers_dir = './__scalers__/'
 
 def get_feature_values(model_id, feature_type_id):
 
@@ -62,134 +63,86 @@ def preprocess(model_id):
     return  df_input, df_target ,df_time_steps_input, df_time_steps_target
 
 
-def scale(X,ref_min, ref_max):
-    range_max = 1
-    range_min = 0
-    
-    X_min = X.min(axis=0).to_frame().transpose()
-    X_max = X.max(axis=0).to_frame().transpose()
-    
-    ref_min = pd.concat([ref_min, X_min], ignore_index=True).min(axis=0)
-    ref_max = pd.concat([ref_max, X_max], ignore_index=True).max(axis=0)
-    
-    X_std = (X - ref_min) / (ref_max - ref_min)
-    X_scaled = (X_std * (range_max - range_min)) + range_min
-    X_scaled = X_scaled.fillna(0)
-    return X_scaled
-
-
-def inverse_scale(X_scaled,ref_min, ref_max):
-    range_max = 1
-    range_min = 0
-    
-
-    X = (((X_scaled - ref_min.values)/(range_max - range_min))*(ref_max.values-ref_min.values))+ref_min.values
-
-    return X
-
-
 def learn(model_id):
-    
     df_input, df_target, df_time_steps_input, df_time_steps_target = preprocess(model_id)
     
     if df_input.shape[0] == 0:
         result = 4
     else:
-        input_tensor_train, input_tensor_test, target_tensor_train, target_tensor_test = train_test_split(df_input, df_target, test_size=test_ratio, shuffle=True)
-        
-        for i in df_time_steps_input.loc["ID"]:
-            min_val = input_tensor_train[i].min()
-            max_val = input_tensor_train[i].max()
-            mean_val = input_tensor_train[i].mean()
-            std_val = input_tensor_train[i].std()
-            
-            sql = "EXEC SP_UPDATE_TIME_STEP "+str(i)+", '"+ str(min_val) +"', '"+ str(max_val) +"','"+ str(mean_val) +"','"+ str(std_val) +"' " 
-            execute_sql(sql,"none")
+        tensor_input_train, tensor_input_test, tensor_target_train, tensor_target_test = train_test_split(df_input, df_target, test_size=test_ratio, shuffle=False)
     
-    
-        for i in df_time_steps_target.loc["ID"]:
-            min_val = target_tensor_train[i].min()
-            max_val = target_tensor_train[i].max()
-            mean_val = target_tensor_train[i].mean()
-            std_val = target_tensor_train[i].std()
-            
-            sql = "EXEC SP_UPDATE_TIME_STEP "+str(i)+", '"+ str(min_val) +"', '"+ str(max_val) +"','"+ str(mean_val) +"','"+ str(std_val) +"' " 
-            execute_sql(sql,"none")
-    
-    
-        sql = "EXEC SP_GET_TIME_STEPS "+ str(model_id) +", 1"
-        df_time_steps_input = execute_sql(sql, "").transpose()
-        
-        sql = "EXEC SP_GET_TIME_STEPS "+ str(model_id) +", 2"
-        df_time_steps_target =  execute_sql(sql, "").transpose()
-        
-        df_test_index = input_tensor_test.index
+        df_test_index = tensor_input_test.index
         df_time_steps_input.columns = df_input.columns
-        df_time_steps_target.columns = df_target.columns
-        ref_min_x = df_time_steps_input.loc[["REFERENCE_MINIMUM"]]
-        ref_max_x = df_time_steps_input.loc[["REFERENCE_MAXIMUM"]]
-        ref_min_y = df_time_steps_target.loc[["REFERENCE_MINIMUM"]]
-        ref_max_y = df_time_steps_target.loc[["REFERENCE_MAXIMUM"]]
+        df_time_steps_target.columns = df_target.columns 
         
         feature_size_x = df_time_steps_input.loc[["MODEL_FEATURE_ID"]].transpose().MODEL_FEATURE_ID.unique().size
         feature_size_y = df_time_steps_target.loc[["MODEL_FEATURE_ID"]].transpose().MODEL_FEATURE_ID.unique().size
+            
+        scaler_input = MinMaxScaler()
+        scaler_target = MinMaxScaler()
         
+        scaler_input.fit(tensor_input_train)
+        scaler_target.fit(tensor_target_train)
         
-        scaled_input_train = scale(input_tensor_train, ref_min_x, ref_max_x)
-        scaled_target_train = scale(target_tensor_train, ref_min_y, ref_max_y)
-        scaled_input_test = scale(input_tensor_test, ref_min_x, ref_max_x)
-        scaled_target_test = scale(target_tensor_test, ref_min_y, ref_max_y)
+        scaled_input_train = scaler_input.transform(tensor_input_train)
+        scaled_target_train = scaler_target.transform(tensor_target_train)
         
-        prediction = Neural_Attention_Mechanism.main(scaled_input_train.values, scaled_target_train.values, scaled_input_test.values, feature_size_x, feature_size_y)
+        scaler_input.partial_fit(tensor_input_test)
+        scaler_target.partial_fit(tensor_target_test)
+        scaled_input_test = scaler_input.transform(tensor_input_test)
         
-        prediction = inverse_scale(prediction, ref_min_y, ref_max_y)
+        prediction = Neural_Attention_Mechanism.main(scaled_input_train, scaled_target_train, scaled_input_test, feature_size_x, feature_size_y)
+        
+        prediction = scaler_target.inverse_transform(prediction)
         prediction = pd.DataFrame(prediction)
         prediction["INDEX"] = df_test_index
         prediction = prediction.set_index("INDEX")
-        prediction.columns = target_tensor_test.columns
+        prediction.columns = tensor_target_test.columns
         
-        actual = target_tensor_test
+        scaler_file_input = scalers_dir +  str(model_id) + ' input.sav'
+        scaler_file_target =scalers_dir +  str(model_id) + ' target.sav'
+        
+        pickle.dump(scaler_input, open(scaler_file_input, 'wb'))
+        pickle.dump(scaler_target, open(scaler_file_target, 'wb'))
+        
+        actual = tensor_target_test
         
         sql ="EXEC SP_DELETE_TESTS " + str(model_id) + "; "
         
-        # for i_index, i_row in df_window_length_target.iterrows():   
-        #     for j_index, j_row in df_features_target.iterrows():
-        #         time_step = i_row[0]
-        #         window_length_min = j_row["WINDOW_LENGTH_MIN"]
-        #         window_length_max = j_row["WINDOW_LENGTH_MAX"]
-        #         if time_step >= window_length_min and time_step <= window_length_max:
-        #             model_feature_id = j_row["ID"]
-        #             feature_name = j_row["FEATURE_SHORT_DESCRIPTION"]
-        #             stream_name = j_row["STREAM_SHORT_DESCRIPTION"]
-        #             col_name = "2"+ "_" + stream_name+ "_" +feature_name+ "_" + str(time_step)
-                                        
-        #             y_true = np.array(actual[col_name])
-        #             y_pred = np.array(prediction[col_name])
+        df_time_steps_target = df_time_steps_target.transpose()
+        for i_index, i_row in df_time_steps_target.iterrows():   
+            boundary = i_row["BOUNDARY"]
+            
+            if boundary == 1:
+                time_step_id = i_row["ID"]
+                                    
+                y_true = np.array(actual[time_step_id])
+                y_pred = np.array(prediction[time_step_id])
+                
+                mse = metrics.mean_squared_error(y_true, y_pred)
+                mae = metrics.mean_absolute_error(y_true, y_pred) 
+                r = np.corrcoef(y_true, y_pred)[0, 1]
+                r2 = r**2
+                mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+                count = len(y_true)
+                exp_var = metrics.explained_variance_score(y_true, y_pred)
                     
-        #             mse = metrics.mean_squared_error(y_true, y_pred)
-        #             mae = metrics.mean_absolute_error(y_true, y_pred) 
-        #             r = np.corrcoef(y_true, y_pred)[0, 1]
-        #             r2 = r**2
-        #             mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
-        #             count = len(y_true)
-        #             exp_var = metrics.explained_variance_score(y_true, y_pred)
-                    
-        #             sql =sql + "EXEC SP_ADD_TEST "+str(model_feature_id)+", "+str(time_step)+", 1 ,"+ str(count) + "; "                    
-        #             sql =sql + "EXEC SP_ADD_TEST "+str(model_feature_id)+", "+str(time_step)+", 2 ,"+ str(r2)+ "; "                    
-        #             sql =sql + "EXEC SP_ADD_TEST "+str(model_feature_id)+", "+str(time_step)+", 3 ,"+ str(mae)+ "; "                         
-        #             sql =sql + "EXEC SP_ADD_TEST "+str(model_feature_id)+", "+str(time_step)+", 4 ,"+ str(mse)+ "; "                    
-        #             sql =sql + "EXEC SP_ADD_TEST "+str(model_feature_id)+", "+str(time_step)+", 5 ,"+ str(mape)+ "; "                    
-        #             sql =sql + "EXEC SP_ADD_TEST "+str(model_feature_id)+", "+str(time_step)+", 6 ,"+ str(exp_var)+ "; "
-        #             sql =sql + "EXEC SP_ADD_TEST "+str(model_feature_id)+", "+str(time_step)+", 7 ,"+ str(r)+ "; "
+                sql =sql + "EXEC SP_ADD_TEST "+str(time_step_id)+", 1 ,"+ str(count) + "; "                    
+                sql =sql + "EXEC SP_ADD_TEST "+str(time_step_id)+", 2 ,"+ str(r2)+ "; "                    
+                sql =sql + "EXEC SP_ADD_TEST "+str(time_step_id)+", 3 ,"+ str(mae)+ "; "                         
+                sql =sql + "EXEC SP_ADD_TEST "+str(time_step_id)+", 4 ,"+ str(mse)+ "; "                    
+                sql =sql + "EXEC SP_ADD_TEST "+str(time_step_id)+", 5 ,"+ str(mape)+ "; "                    
+                sql =sql + "EXEC SP_ADD_TEST "+str(time_step_id)+", 6 ,"+ str(exp_var)+ "; "
+                sql =sql + "EXEC SP_ADD_TEST "+str(time_step_id)+", 7 ,"+ str(r)+ "; "
 
-        # execute_sql(sql,"none")
-        # result = 5
+    execute_sql(sql,"none")
+    result = 5
         
     return result
 
 
 def main():
-    sql ="SELECT * FROM VW_MODELS WHERE LATEST_STATUS_ID = 2"
+    sql ="SELECT * FROM VW_MODELS --WHERE LATEST_STATUS_ID = 2"
     
     df_models = execute_sql(sql)
     for i_index, i_row in df_models.iterrows():
@@ -199,10 +152,4 @@ def main():
         execute_sql("EXEC SP_UPDATE_MODEL_STATUS '"+str(model_id)+"', "+ str(result) +", 1")
         
     
-# main()
-
-
-model_id = 1068
-
-        
-        
+main()
