69e970ee8610cad549410461aca47c7a589557f0
emremidilli <70489609+emremidilli@users.noreply.github.com>
2020-10-15 10:35:44 +0300

    Update Neural_Attention_Mechanism.py

--- Neural_Attention_Mechanism.py
+++ Neural_Attention_Mechanism.py
@@ -47,7 +47,6 @@ class Encoder(tf.keras.Model):
                                     recurrent_initializer='glorot_uniform',activation = activation_function,dropout = dropout_rate , recurrent_dropout= recurrent_dropout_rate)
     
   def call(self, x, hidden): #incoming X must be a matrix since embedding layer is cancelled. Matrix shape is (backward_window_length, backward_feature_size) 
-    
     output, state = self.gru(x, initial_state = hidden)
     return output, state
 
@@ -106,7 +105,6 @@ class Decoder(tf.keras.Model):
     # enc_output shape == (batch_size, max_length, hidden_size)
     context_vector, attention_weights = self.attention(hidden, enc_output)
 
-
     x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
 
     # passing the concatenated vector to the GRU
@@ -137,23 +135,25 @@ def train_step(inp, targ, enc_hidden, p_encoder, p_decoder):
     for t in range(1, targ.shape[1]):
       # passing enc_output to the decoder
       predictions, dec_hidden, _ = p_decoder(dec_input, dec_hidden, enc_output)
-
+      
       loss += loss_function(targ[:, t], predictions)
 
       # using teacher forcing
       dec_input = tf.expand_dims(predictions, 1)
-
+      
+  
   batch_loss = (loss / int(targ.shape[1]))
-
+  
   variables = p_encoder.trainable_variables + p_decoder.trainable_variables
-
+  
   gradients = tape.gradient(loss, variables)
-
+  
   optimizer.apply_gradients(zip(gradients, variables))
-
+  
+  
   return batch_loss
 
-
+@tf.function
 def attention_model(input_tensor_train, target_tensor_train, input_tensor_test):
     global batch_size
     global encoder
@@ -167,15 +167,15 @@ def attention_model(input_tensor_train, target_tensor_train, input_tensor_test):
     
     encoder = Encoder(feature_size_input, number_of_hidden_neuron, batch_size)
     decoder = Decoder(feature_size_target, number_of_hidden_neuron, batch_size)
-    
+
     checkpoint = tf.train.Checkpoint(optimizer=optimizer,encoder=encoder,decoder=decoder)
-    
+
     for epoch in range(epoch_size):
         start = time.time()
         
         encoder_hidden = encoder.initialize_hidden_state()
-        total_loss = 0
-        
+        total_loss = 0.0
+                
         for (batch, (inp, targ)) in enumerate(dataset.take(steps_per_epoch)):         
             # normalize
             inp = tf.reshape(inp, (batch_size, backward_window_length, feature_size_input, 1))
@@ -183,17 +183,21 @@ def attention_model(input_tensor_train, target_tensor_train, input_tensor_test):
             
             inp = tf.reshape(inp, (batch_size, backward_window_length, feature_size_input))
             targ = tf.reshape(targ, (batch_size, forward_window_length,feature_size_target))
-
+            
             batch_loss = train_step(inp, targ, encoder_hidden, encoder, decoder)
+            
+            print(total_loss)
+            print(batch_loss)
             total_loss += batch_loss
+            
             # denormalize
 
         # saving (checkpoint) the model every 2 epochs
         if (epoch + 1) % 2 == 0:
             checkpoint.save(file_prefix = checkpoint_prefix)
             
-        print('Epoch {} Loss {:.4f}'.format(epoch + 1,total_loss / steps_per_epoch))
-        print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+        # print('Epoch {} Loss {:.4f}'.format(epoch + 1,total_loss / steps_per_epoch))
+        # print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
         
         # restoring the latest checkpoint in checkpoint_dir
         checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
@@ -201,49 +205,40 @@ def attention_model(input_tensor_train, target_tensor_train, input_tensor_test):
     return attention_predict(input_tensor_test)
     
 
+@tf.function
 def attention_predict(input_tensor_test):
     global encoder
     global decoder
-
+    
     batch_size_test = len(input_tensor_test)
-        
+    
     dataset_test = (tf.data.Dataset.from_tensor_slices((input_tensor_test)))
     
     dataset_test = dataset_test.batch(batch_size_test, drop_remainder=True)
     
-    encoder_hidden = [tf.zeros((batch_size_test, number_of_hidden_neuron))]
-    steps_per_epoch_test = len(input_tensor_test)//batch_size_test
-    
-    for (batch, (inp_test)) in enumerate(dataset_test.take(steps_per_epoch_test)):  
+    encoder_hidden = tf.zeros((batch_size_test, number_of_hidden_neuron))
+
+    predictions = tf.zeros(1)
+    for (batch, (inp_test)) in enumerate(dataset_test.take(-1)):
         inp_test = tf.reshape(inp_test, (batch_size_test, backward_window_length, feature_size_input, 1))
-     
         inp_test = tf.reshape(inp_test, (batch_size_test, backward_window_length, feature_size_input))
-        
+
         encoder_output, encoder_hidden = encoder(inp_test, encoder_hidden)
         decoder_hidden = encoder_hidden
-
+        
+        decoder_input = tf.expand_dims(np.zeros((batch_size_test,feature_size_target)) , 1)
+        
+        p2 = tf.zeros(1)
         for t in range(0, feature_size_target*forward_window_length):
-            if t == 0:
-                decoder_input = tf.expand_dims(np.zeros((batch_size_test,feature_size_target)) , 1)
-            else:
-                decoder_input = tf.expand_dims(p, 1)
-
             # passing enc_output to the decoder
             p, dec_hidden, _ = decoder(decoder_input, decoder_hidden, encoder_output)
-            
+            decoder_input = tf.expand_dims(p, 1)
             if t == 0:
                 p2 = p
             else:
                 p2 = tf.concat([p2, p],1)
-
-        if batch == 0:
-            predictions = p2
-            
-        else:
-            predictions = tf.concat([predictions, p2],0)
-       
-    predictions = np.array(predictions)
-
+        
+        predictions = p2
     return predictions
 
 
