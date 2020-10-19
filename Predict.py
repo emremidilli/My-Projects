d87c8129482740825641d5bc2adae4251656b6c4
emremidilli <70489609+emremidilli@users.noreply.github.com>
2020-10-19 11:53:50 +0300

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -9,6 +9,7 @@ import Train_Models
 from Connect_to_Database import execute_sql
 import Neural_Attention_Mechanism
 import pickle
+import pandas as pd
 
 
 def main():
@@ -16,8 +17,14 @@ def main():
     df_models = execute_sql(sql, "")
     for i_index, i_row in df_models.iterrows():
         model_id = i_row["ID"]
-        df_input, df_time_steps_input= Train_Models.get_feature_values(model_id, "1")
+        df_input, df_time_steps_input = Train_Models.get_feature_values(model_id, "1",True)
+        df_output , df_time_steps_target = Train_Models.get_feature_values(model_id, "2", False)
         
+        feature_size_x, window_length_x = Train_Models.get_dimension_size(df_time_steps_input)
+        feature_size_y , window_length_y = Train_Models.get_dimension_size(df_time_steps_target)
+        
+        df_input_index = df_input.index
+         
         sql = "SELECT * FROM FN_GET_MODEL_FEATURES("+ str(model_id) +", 2)"
         df_output_model_features = execute_sql(sql, "")
         for j_index, j_row in df_output_model_features.iterrows():
@@ -26,25 +33,39 @@ def main():
             sql = "EXEC SP_GET_FEATURE_VALUES_TO_BE_PREDICTED " + str(model_feature_id)
             
             df_to_be_predicted = execute_sql(sql, "")
-            df_input = df_input[~df_input.index.isin(df_to_be_predicted.index)]
+            df_to_be_predicted = df_to_be_predicted.set_index("TIME_STAMP")
             
-            scaler_file_input = Train_Models.scalers_dir +  str(model_id) + ' input.sav'
-            scaler_file_target =Train_Models.scalers_dir +  str(model_id) + ' target.sav'
+            df_input = df_input[df_input.index.isin(df_to_be_predicted.index)] #~
             
-            scaler_input = pickle.load(open(scaler_file_input, 'rb'))
-            scaler_target = pickle.load(open(scaler_file_target, 'rb'))
-            
-            scaler_input.partial_fit(df_input)
-            
-            scaled_input = scaler_input.transform(df_input)
-            
-            # predicted_result = Neural_Attention_Mechanism.attention_predict(scaled_input)
-            # pickle.dump(scaler_input, open(scaler_file_input, 'wb'))
-            # pickle.dump(scaler_target, open(scaler_file_target, 'wb'))
-            
-            return scaled_input
-            
-            
-scaled_input = main()
+            if df_input.shape[0] > 0:
+                scaler_file_input = Train_Models.scalers_dir +  str(model_id) + ' input.sav'
+                scaler_file_target = Train_Models.scalers_dir +  str(model_id) + ' target.sav'
+                
+                scaler_input = pickle.load(open(scaler_file_input, 'rb'))
+                scaler_target = pickle.load(open(scaler_file_target, 'rb'))
+                
+                scaler_input.partial_fit(df_input)
+                
+                scaled_input = scaler_input.transform(df_input)
+                
+                prediction = Neural_Attention_Mechanism.attention_predict(scaled_input, model_id, feature_size_x, feature_size_y, window_length_x, window_length_y)
+                
+                prediction = scaler_target.inverse_transform(prediction)
+                prediction = pd.DataFrame(prediction)
+                prediction["INDEX"] = df_input_index
+                prediction = prediction.set_index("INDEX")
+                prediction.columns = df_output.columns
+                
+                
+                for l_index, l_row in prediction.iterrows():
+                    base_feature_value_id =df_to_be_predicted.loc[l_index]["ID"]
+                    for time_step_id , predicted_value in l_row.iteritems():
+                        sql_add_prediction = "EXEC SP_ADD_PREDICTION "+str(base_feature_value_id)+" ,"+str(time_step_id)+" , '"+str(predicted_value)+"' "
+                        execute_sql(sql_add_prediction,"none")
 
-
+                
+                pickle.dump(scaler_input, open(scaler_file_input, 'wb'))
+                pickle.dump(scaler_target, open(scaler_file_target, 'wb'))
+                
+            
+main()
